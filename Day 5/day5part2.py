7199091f26fd18d8e1aade063e608c5858500456
amphin <72383772+amphin@users.noreply.github.com>
2020-12-05 15:07:21 +0000

    Add files via upload

---
+++ Day 5/day5part2.py
@@ -0,0 +1,28 @@
+import os, sys
+
+with open(os.path.join(sys.path[0], "day5input.txt")) as file:
+    passes = file.read().splitlines()
+
+def find(bpass, index, min, max):
+    # F/L = lower half, B/R = upper half
+    diff = (max + 1) - min
+    if bpass[index] == "F" or bpass[index] == "L":
+        if diff == 2:
+            return min
+        return find(bpass, index + 1, min, max - diff/2)
+    else: # == "B" or "R":
+        if diff == 2:
+            return max
+        return find(bpass, index + 1, min + diff/2, max)
+
+# main
+ids = []
+for bpass in passes:
+    ids.append((int(find(bpass, 0, 0, 127)) * 8) + int(find(bpass, 7, 0, 7)))
+
+ids.sort()
+diff = 13
+for i in range(0, len(ids)):
+    if (i != ids[i] - diff):
+        print(ids[i] - 1)
+        diff += 1
